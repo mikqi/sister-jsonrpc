9bba1471cd54a08457be63ecf9ba864a784aafdf
Muhammad Rivki <Muhammad.Rivki@outlook.com>
2016-01-17 20:11:17 +0700

    tambah prompt buat tanya nama

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,21 @@
+alertify.defaults.transition = 'zoom';
+alertify.defaults.title = 'Selamat datang';
+alertify.set('notifier', 'position', 'top-right');
+
+// alertify.defaults.theme.ok = 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent';
+alertify.defaults.theme.cancel = 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect';
+alertify.prompt('Masukkan nama anda', 'Joko', function(evt, value) {
+    alertify.success('Selamat datang ' + value);
+    localStorage.setItem('nama', value);
+  },
+
+  function() {
+    localStorage.setItem('nama', 'default');
+    alertify.error('nama default anda default');
+  });
+
+$('.ajs-header')[0].innerHTML = 'Selamat Datang!';
+
 var socket = io();
 $('form').submit(function() {
   socket.emit('chat message', $('#message').val());
@@ -7,7 +25,11 @@ $('form').submit(function() {
 
 socket.on('chat message', function(msg) {
   console.log(msg);
-  $('#messages').append('<div class="mdl-card mdl-shadow--2dp"> <div class="mdl-card__title mdl-card--expand"> <p>' + msg[msg.length - 1].result.pesan + '</p><p class="end">' + msg[msg.length - 1].result.waktu + '</p> </div> </div>');
+  $('#messages').append(`<div class="mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title mdl-card--expand">
+    <p> ${msg[msg.length - 1].result.pesan} </p>
+    <p class="end"> ${msg[msg.length - 1].result.waktu} </p>
+  </div> </div>`);
 
 });
 
